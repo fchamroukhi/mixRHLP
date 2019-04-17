4a2b8a58da74fb91c6aeb776a011403acbcd07a2
Marius <marius.bartcus@gmail.com>
2019-04-17 17:10:08 +0200

    ModelMixRHLP

---
+++ MixRHLP/R/ModelMixRHLP.R
@@ -0,0 +1,43 @@
+source("R/FData.R")
+source("R/enums.R")
+
+ModelMixRHLP <- setRefClass(
+  "ModelMixRHLP",
+  contains = "FData",
+  # Define the fields
+  fields = list(
+    G = "numeric",
+    # number of clusters
+    K = "numeric",
+    # number of regimes
+    p = "numeric",
+    # dimension of beta (order of polynomial regression)
+    q = "numeric",
+    # dimension of w (order of logistic regression)
+    variance_type = "numeric",
+    nu = "numeric" # degree of freedom
+  )
+)
+
+ModelMixRHLP <- function(fData, G, K, p, q, variance_type) {
+  if (variance_type == variance_types$homoskedastic) {
+    nu <<- G * ((p + q + 3) * K - (q + 1) - (K - 1))
+  }
+  else{
+    nu <<- G * ((p + q + 3) * K - (q + 1))
+  }
+
+  new(
+    "ModelMixRHLP",
+    Y = fData$Y,
+    X = fData$X,
+    m = fData$m,
+    n = fData$n,
+    G = G,
+    K = K,
+    p = p,
+    q = q,
+    variance_type = variance_type,
+    nu = nu
+  )
+}
