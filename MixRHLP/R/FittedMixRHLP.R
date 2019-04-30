556d7bec2937aade04bf5bc9e5b32780a3cac4f5
Florian Lecocq <florian.lecocq@outlook.com>
2019-04-30 16:46:23 +0200

    Improve plots

--- MixRHLP/R/FittedMixRHLP.R
+++ MixRHLP/R/FittedMixRHLP.R
@@ -8,48 +8,47 @@ FittedMixRHLP <- setRefClass(
   methods = list(
     plot = function() {
 
-      #### cluster and means
-      couleur = rainbow(modelMixRHLP$G)
-      par(mfrow=c(round(sqrt(modelMixRHLP$G + 1)), round(sqrt(modelMixRHLP$G + 1))))
+      # yaxislim <- c(min(modelMixRHLP$Y) - 2 * mean(sqrt(apply(modelMixRHLP$Y, 1, var))), max(modelMixRHLP$Y) + 2 * mean(sqrt(apply(modelMixRHLP$Y, 1, var))))
 
-      matplot(t(modelMixRHLP$Y), col = "black", type = "l", lty = "solid", xlab = "Time", ylab = "y")
-      title(main = "Initial datset")
+      # Cluster and means
+      colorsvector = rainbow(modelMixRHLP$G)
+      par(mfrow=c(round(sqrt(modelMixRHLP$G + 1)), round(sqrt(modelMixRHLP$G + 1))), mai = c(0.6, 0.6, 0.5, 0.25), mgp = c(2, 1, 0))
+
+      matplot(t(modelMixRHLP$Y), type = "l", lty = "solid", col = "black", xlab = "Time", ylab = "y(t)")
+      title(main = "Original time series")
 
       for (g in 1:modelMixRHLP$G) {
         cluster_g = modelMixRHLP$Y[statMixRHLP$klas == g,]
-        matplot(t(cluster_g), col = couleur[g], type = "l", lty = "dotted", xlab = "Time", ylab = "y")
+        matplot(t(cluster_g), type = "l", lty = "dotted", col = colorsvector[g], xlab = "Time", ylab = "y(t)")
         if (is.null(dim(statMixRHLP$Ex_g))) {
-          lines(statMixRHLP$Ex_g, lty = "solid", lwd = 2, col = "black")
+          lines(statMixRHLP$Ex_g, col = "black", lty = "solid", lwd = 1.5)
         } else {
-          lines(statMixRHLP$Ex_g[, g], lty = "solid", lwd = 2, col = "black")
+          lines(statMixRHLP$Ex_g[, g], col = "black", lty = "solid", lwd = 1.5)
         }
         title(main = sprintf("Cluster %1.1i", g))
       }
 
-      par(mfrow = c(2, 1))
+      par(mfrow = c(2, 1), mai = c(0.6, 0.8, 0.5, 0.5))
       for (g in 1:modelMixRHLP$G) {
 
-        # First graph
         cluster_g = modelMixRHLP$Y[statMixRHLP$klas == g,]
-        matplot(t(cluster_g), col = couleur[g], type = "l", lty = "dotted", xlab = "Time", ylab = "y")
-        # polynomial regressors
+        matplot(t(cluster_g), type = "l", lty = "dotted", col = colorsvector[g], xlab = "Time", ylab = "y(t)")
+        # Polynomial regressors
         for (i in 1:ncol(statMixRHLP$polynomials[, , g])) {
-          lines(statMixRHLP$polynomials[, i, g], lty = "dotted", lwd = 2, col = "black")
+          lines(statMixRHLP$polynomials[, i, g], col = "black", lty = "dotted", lwd = 1.5)
         }
         if (is.null(dim(statMixRHLP$Ex_g))) {
-          lines(statMixRHLP$Ex_g, lty = "solid", lwd = 2, col = "black")
+          lines(statMixRHLP$Ex_g, col = "black", lty = "solid", lwd = 1.5)
         } else {
-          lines(statMixRHLP$Ex_g[, g], lty = "solid", lwd = 2, col = "black")
+          lines(statMixRHLP$Ex_g[, g], col = "black", lty = "solid", lwd = 1.5)
         }
         title(main = sprintf("Cluster %1.1i", g))
 
-        matplot(paramMixRHLP$pi_jgk[1:modelMixRHLP$m, , g], type = "l", lty = "solid",
-                xlab = "Time", ylab = "Logistic proportions") # ['\pi_{jk}( w^g) , g = ',int2str(g)])
+        matplot(paramMixRHLP$pi_jgk[1:modelMixRHLP$m, , g], type = "l", lty = "solid", xlab = "Time", ylab = "Logistic proportions")
 
       }
 
       par(mfrow = c(1, 1))
-
       plot.default(unlist(statMixRHLP$stored_loglik), type = "l", col = "blue", xlab = "Iteration", ylab = "Log Likelihood")
       title(main = "Log-Likelihood")
 
