875dcac65c17e2f1e6c2aa98e4873016382cd72c
Florian Lecocq <florian.lecocq@outlook.com>
2019-06-13 11:22:56 +0200

    Add a summary method to ModelMixRHLP class
    Add a method 'summary' to the class ModelMixRHLP

--- R/ModelMixRHLP.R
+++ R/ModelMixRHLP.R
@@ -55,7 +55,75 @@ ModelMixRHLP <- setRefClass(
       plot.default(unlist(statMixRHLP$stored_loglik), type = "l", col = "blue", xlab = "Iteration", ylab = "Log Likelihood")
       title(main = "Log-Likelihood")
 
+    },
+
+    summary = function() {
+
+      digits = getOption("digits")
+
+      title <- paste("Fitted mixRHLP model")
+      txt <- paste(rep("-", min(nchar(title) + 4, getOption("width"))), collapse = "")
+
+      # Title
+      cat(txt)
+      cat("\n")
+      cat(title)
+      cat("\n")
+      cat(txt)
+
+      cat("\n")
+      cat("\n")
+      cat(paste0("MixRHLP model with G = ", paramMixRHLP$G, ifelse(paramMixRHLP$G > 1, " clusters", " cluster"),
+                 " and K = ", paramMixRHLP$K, ifelse(paramMixRHLP$K > 1, " regimes", " regime"), ":"))
+      cat("\n")
+      cat("\n")
+
+      tab <- data.frame("log-likelihood" = statMixRHLP$log_lik, "nu" = paramMixRHLP$nu, "AIC" = statMixRHLP$AIC,
+                        "BIC" = statMixRHLP$BIC, "ICL" = statMixRHLP$ICL, row.names = "", check.names = FALSE)
+      print(tab, digits = digits)
+
+      cat("\nClustering table:")
+      print(table(statMixRHLP$klas))
+
+      cat("\nMixing probabilities (cluster weights):\n")
+      pro <- data.frame(paramMixRHLP$alpha_g)
+      colnames(pro) <- 1:paramMixRHLP$G
+      print(pro, digits = digits, row.names = FALSE)
+
+      cat("\n\n")
+
+      txt <- paste(rep("-", min(nchar(title), getOption("width"))), collapse = "")
+
+      for (g in 1:paramMixRHLP$G) {
+        cat(txt)
+        cat("\nCluster ", g, " (G = ", g, "):\n", sep = "")
+
+        cat("\nRegression coefficients:\n\n")
+        if (paramMixRHLP$p > 0) {
+          row.names = c("1", sapply(1:paramMixRHLP$p, function(x) paste0("X^", x)))
+        } else {
+          row.names = "1"
+        }
+
+        betas <- data.frame(paramMixRHLP$betag[, , g], row.names = row.names)
+        colnames(betas) <- sapply(1:paramMixRHLP$K, function(x) paste0("Beta(K = ", x, ")"))
+        print(betas, digits = digits)
+
+        cat(paste0(ifelse(paramMixRHLP$variance_type == variance_types$homoskedastic, "\n",
+                          "\nVariances:\n\n")))
+        sigma2 <- data.frame(t(paramMixRHLP$sigmag[, g]))
+        if (paramMixRHLP$variance_type == variance_types$homoskedastic) {
+          colnames(sigma2) <- "Sigma2"
+          print(sigma2, digits = digits, row.names = FALSE)
+        } else {
+          colnames(sigma2) = sapply(1:paramMixRHLP$K, function(x) paste0("Sigma2(K = ", x, ")"))
+          print(sigma2, digits = digits, row.names = FALSE)
+        }
+        cat("\n")
+      }
+
     }
+
   )
 )
 
